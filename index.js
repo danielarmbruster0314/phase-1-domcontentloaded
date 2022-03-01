bd909d9afe67da09188c9bcfd31125ab1419771d
Daniel Armbruster <danielgundam00@gmail.com>
2022-03-01 10:48:24 -0700

    commit tree 524944ed57361fd2518db95249711df1cca010f6

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Your code goes here
+ document.addEventListener( "DOMContentLoaded", function () {
+     why()
+ });
+
+ function why(){
+     document.getElementById( "text" )
+      .innerHTML = "This is really cool!";
+ }
+
+
+
